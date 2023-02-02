7b75dfff692df97bbabfb7ad31f93b5e393e4791
Ezioko <114493990+Ezioko@users.noreply.github.com>
2023-02-02 02:36:56 +0100

    email service

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { Email } from './models/email.model'
+import { AuthService } from './auth.service'
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class EmailService {
+	BASE_URL = 'http://localhost:8080/api/emails'
 
-  constructor() { }
+	private get headerOptions() {
+		return { headers: { 'Authorization': `Bearer ${this.auth.token}` } }
+	}
+
+	constructor(private httpClient: HttpClient, private readonly auth: AuthService) {
+	}
+
+	findEmails(): Observable<Email[]> {
+		return this.httpClient.get<Email[]>(this.BASE_URL, this.headerOptions)
+	}
+
+	findEmailById(id: number): Observable<Email> {
+		return this.httpClient.get<Email>(this.BASE_URL + '/' + id, this.headerOptions)
+	}
+
+	deleteEmailById(id: number): Observable<Object> {
+		return this.httpClient.delete(this.BASE_URL + '/' + id, this.headerOptions)
+	}
+
+	resendEmailById(id: number) {
+		return this.httpClient.post(this.BASE_URL + '/' + id + '/resend', null, this.headerOptions)
+	}
 }
